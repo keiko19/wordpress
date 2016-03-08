a656c13c41917fa54f13b384343448981d8bf8af
liuhuizi min <liuhuizi.min@qunar.com>
2016-03-08 11:26:25 +0800

    page style

--- footer.php
+++ footer.php
@@ -1,50 +1,6 @@
     <footer id="footer">
-        <!--上下翻页 start-->
-        <div class="wp_slide_pager">
-        <div class="wp_slide_pager prev">Scroll to back</div>
-        <div class="wp_slide_pager next">Scroll to know more</div>
-        </div>
-        <!--上下翻页 end /-->
-        <!--左侧main nav start-->
-        <div class="sidebar-contentWrapper">
-            <?php get_sidebar(); ?>
-        </div>
-        <div class="banner">
-            <!--搜索 start-->
-            <div class="search">
-                <div class="big_seach_icon"></div>
-                <div class="seach_form">
-                    <i class="small_search_icon"></i>
-                    <input value=""/>
-                </div>
-            </div>
-            <!--搜索 end /-->
-            <!--博客简介 start-->
-            <div class="wp_intro">
-                <dl>
-                    <dt class="wp_img"></dt>
-                    <dd class="wp_link">
-                        <p>EVACUEE</p>
-                        <p>***.COM</p>
-                    </dd>
-                </dl>
-            </div>
-            <!--博客简介 end /-->
-            <!--文章简介 start-->
-            <div class="wp_page_intro">
-                <dl>
-                    <dt class="wp_pager"></dt>
-                    <dd class="wp_preview">
-                        <p class="title">EVACUEE</p>
-                        <p class="inner_preview">***.COM</p>
-                    </dd>
-                </dl>
-            </div>
-            <!--文章简介 end /-->
-        </div>
     </footer>
-    <!--<script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.10.2.min.js"></script>-->
-    <!--script src="<?php bloginfo('template_directory'); ?>/js/jquery.tmpl.min.js"></script-->
-    <!--<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>-->
+    <script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.10.2.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,14 +1,54 @@
 <?php get_header(); ?>
-<!--右侧nav start-->
-<div class="wp_nav">
-    <ul>
-        <li class="wp_home"></li>
-        <li class="wp_home_thumb"></li>
-        <li class="wp_nav_pager">
-            <span class="prev"></span>
-            <span class="next"></span>
-        </li>
-    </ul>
+<div class="body">
+    <div class="top">
+        <div class="mylogo pointer"></div>
+        <div class="nav pointer iconfont">&#xe603;</div>
+    </div>
+    <div class="center">
+        <div class="prev pointer"><div class="wrap"><span class="btn"></span><span class="btn-txt">PREV</span></div></div>
+        <div class="next pointer"><div class="wrap"><span class="btn"></span><span class="btn-txt">NEXT</span></div></div>
+        <div class="image">
+            <div class="mask"></div>
+            <div class="picnum">15P</div>
+            <img class="main-img" src="lala.jpg">
+            <div class="enter pointer">enter</div>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="brief">
+            酒店宽敞客房的宽大窗户高出新宿区热闹的街道，可欣赏到富士山或新宿区的美景。内部拥有室内游泳池和一家位于52层的餐厅。酒店的客房拥有北海道木镶板和埃及棉床单。宾客能在吧台品尝美酒，在深深的浴缸放松身心，或穿着松软的浴衣欣赏点播电影。新宿中央公园距离酒店有3分钟步行路程。J R新宿火车站距离..
+        </div>
+        <div class="info">
+            <p class="title">Sri Lank</p>
+            <div class="time">
+                <div class="year">15</div>
+                <div class="date">12/13</div>
+            </div>
+        </div>
+    </div>
+    <!-- 导航 start -->
+    <div class="thunm">
+        <div class="main-mask"></div>
+        <div class="nav-list">
+            <div class="close iconfont pointer">&#xe605</div>
+            <div class="social">
+                <div class="search">
+                    <i class="magnifier iconfont pointer">&#xe600;</i>
+                    <input name="key"/>
+                </div>
+                <ul class="list">
+                    <li>ABOUT</li>
+                    <li>LINKS</li>
+                    <li>ACTIVITY</li>
+                </ul>
+            </div>
+            <ul class="thumb-list">
+                <li>DIARY</li>
+                <li>LINKS</li>
+                <li>ACTIVITY</li>
+            </ul>
+        </div>
+    </div>
+    <!-- 导航 end -->
 </div>
-        <!--右侧nav end /-->
 <?php get_footer(); ?>
