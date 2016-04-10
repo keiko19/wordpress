f04f4c1915955b7fac90c3ff132ce07bdce24e6c
liuhuizi min <liuhuizi.min@qunar.com>
2016-04-10 22:40:42 +0800

    no message

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php get_header(); ?>
+<?php if ( is_home() && ! is_front_page() ) : ?>
+<header>
+    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+</header>
+<?php endif; ?>
 <div class="body">
     <div class="top">
         <div class="mylogo pointer"></div>
