59218484174b10f3c48a6770aa7f27e974d1f0c0
liuhuizi min <liuhuizi.min@qunar.com>
2016-05-05 18:53:02 +0800

    index

--- js/script.js
+++ js/script.js
@@ -1,21 +1,139 @@
-var TPL = ['{{each data as item}}<li class="item" data-index="{{item.index}}"><div class="image">',
-    '<div class="mask"></div>',
-    '<div class="enter pointer" data-src="{{item.url}}"><a class="enter-link" target="_blank" href="{{item.url}}">enter</a></div>',
-    '<div class="picnum">{{item.picnum}}P</div>',
-    '<img class="main-img" src="{{item.attachmentsUrl}}">',
-    '</div>',
-    '</div>',
-    '<div class="bottom">',
-    '<div class="brief">{{item.excerpt}}</div>',
-    '<div class="info">',
-    '<p class="title">{{item.title}}</p>',
-    '<div class="time"><div class="year">{{item.year}}</div><div class="date">{{item.month}}/{{item.day}}</div></div></div>',
-    '</div></li>{{/each}}'].join('');
 
-var INDEX = 0; //默认从几开始渲染首页
-var postResult = [];
-var eventDispatcher = {};
+
+var homePage = function(){
+    this.INDEX = 0; //默认从几开始渲染首页
+    this.postResult = [];
+    this.animateDiv = $('#container');
+    this.bindEvent();
+};
+homePage.prototype = {
+    TPL : ['{{each data as item}}<li class="item" data-index="{{item.index}}"><div class="image">',
+            '<div class="mask"></div>',
+            '<div class="enter pointer" data-src="{{item.url}}"><a class="enter-link" target="_blank" href="{{item.url}}">enter</a></div>',
+            '<div class="picnum">{{item.picnum}}P</div>',
+            '<img class="main-img" src="{{item.attachmentsUrl}}">',
+            '</div>',
+            '</div>',
+            '<div class="bottom">',
+            '<div class="brief">{{item.excerpt}}</div>',
+            '<div class="info">',
+            '<p class="title">{{item.title}}</p>',
+            '<div class="time"><div class="year">{{item.year}}</div><div class="date">{{item.month}}/{{item.day}}</div></div></div>',
+            '</div></li>{{/each}}'].join(''),
+    init : function(){
+        var me = this;
+        var recentPost = fetchData({
+            url : "/wordpress/?json=get_recent_posts",
+            data : {page:1,count:10}
+            //data : {page:1,count:INDEX+2}
+        });
+        recentPost.then(function(result){
+            if (result.posts && result.posts.length != 0) {
+                me.postResult = result.posts;
+                var _html = me.renderHomePage( );
+                $('#container').html( _html );
+            }
+        })
+    },
+    getMorePosts : function(){
+        fetchData({
+            url : "/wordpress/?json=get_recent_posts",
+            data : {page:1,count:me.INDEX+2}
+        }).then(function(result){
+            if (result.posts && result.posts.length != 0) {
+                me.postResult = result.posts;
+                me.renderBtns();
+            }
+        });
+    },
+    formatHomeData : function(current,idx){
+        var d = /(\d*)-(\d*)-(\d*)/.exec(current.date);
+        return {
+            index : idx,
+            title: current.title,
+            url: current.url,
+            excerpt: current.excerpt,
+            year: d[1].slice(2),
+            month: d[2],
+            day: d[3],
+            attachmentsUrl: current.attachments.length > 0 && current.attachments[0].images.large.url || "",
+            picnum: current.attachments.length
+        };
+    },
+    renderHomePage : function() {
+        //渲染list
+        var renerData = [],
+            me = this;;
+        this.postResult.forEach(function(current,idx){
+            renerData.push( me.formatHomeData(current,idx));
+        });
+        var tpl = template(me.TPL, { data :renerData });
+        return tpl;
+    },
+    renderBtns : function(){
+        typeof this.postResult[this.INDEX+1] == "undefined" ? $('.js-next').hide() :  $('.js-next').show();
+        typeof this.postResult[this.INDEX-1] == "undefined" ? $('.js-prev').hide() :  $('.js-prev').show();
+    },
+    translates :function(dom, transitionSpeed, translate3dX) {
+        dom[0].style.webkitTransform = 'translate(' + translate3dX + 'px,0px) translateZ(0)';
+        dom[0].style.transform = 'translate(' + translate3dX + 'px, 0px) translateZ(0)';
+
+    },
+    getTranslateX : function(elem){
+        var matrix = window.getComputedStyle(elem[0], null);
+        var transform = matrix['webkitTransform'] || matrix['transform'];
+        var split = transform.split(')')[0].split(', ');
+        var x = Math.round(+(split[12] || split[4]));
+        return x || 0;
+    },
+
+    bindEvent : function(){
+        var me = this;
+        var panel = $('#panel');
+        var nav = $('.js-nav');
+        var thumb = $('.js-thumb');
+        panel.delegate('.js-next', 'click', function(e){
+            var next = me.INDEX+1;
+            var nextDom = me.animateDiv.find('.item[data-index="'+next+'"]');
+            //getMorePosts();
+            if(me.INDEX >=0 && next < me.postResult.length && nextDom.length>0){
+                ++me.INDEX;
+                me.renderBtns();
+                var _x = -me.getTranslateX(me.animateDiv)+nextDom.offset().left -100;
+                me.translates( me.animateDiv, 200, -_x);
+            }
+        }).delegate('.js-prev', 'click', function(e){
+            var prev = me.INDEX-1;
+            var prevDom = me.animateDiv.find('.item[data-index="'+prev+'"]');
+
+            if(prev >=0 && prevDom.length > 0){
+                --me.INDEX;
+                me.renderBtns();
+                var _x = -me.getTranslateX(me.animateDiv)+prevDom.offset().left-100;
+                me.translates( me.animateDiv, 200, -_x);
+            }
+        });
+
+        $(window).bind('resize',function(){
+            var currentDom = me.animateDiv.find('.item[data-index="'+me.INDEX+'"]');
+            if(currentDom.offset().left !== 100){
+                var _x = -me.getTranslateX(me.animateDiv) + currentDom.offset().left - 100;
+                me.translates( me.animateDiv, 200, -_x);
+            }
+        });
+
+        nav.bind('click', function (e) {
+            thumb.show();
+        });
+        thumb.delegate('.js-close', 'click', function(){
+            thumb.hide()
+        });
+    }
+}
+
+
 //解析url
+var homepage = new homePage();
 function initPage(){
     var path = location.pathname;
     var localprefix = 'wordpress',localtest = path.indexOf(localprefix);
@@ -29,20 +147,7 @@ function initPage(){
         var slug = (/\/([^\/]*)\//).exec(path),
             type = 'post';
         if(!slug || path == '/'){
-            var recentPost = fetchData({
-                url : "/wordpress/?json=get_recent_posts",
-                data : {page:1,count:10}
-                //data : {page:1,count:INDEX+2}
-            });
-            recentPost.then(function(result){
-                if (result.posts && result.posts.length != 0) {
-                    postResult = result.posts;
-                    var _html = renderHomePage( postResult );
-                    console.log(postResult)
-                    //var _html = renderHomePage( postResult[INDEX] );
-                    $('#container').html( _html );
-                }
-            });
+            homepage.init();
         }else{
             fetchData({
                 url : "/api/get_"+type+"/",
@@ -99,104 +204,4 @@ function fetchData(json){
     });
 }
 
-function getMorePosts(){
-    fetchData({
-        url : "/wordpress/?json=get_recent_posts",
-        data : {page:1,count:INDEX+2}
-    }).then(function(result){
-        if (result.posts && result.posts.length != 0) {
-            postResult = result.posts;
-            renderBtns();
-        }
-    });
-}
-
-function formatHomeData(current,idx){
-    var d = /(\d*)-(\d*)-(\d*)/.exec(current.date);
-    return {
-        index : idx,
-        title: current.title,
-        url: current.url,
-        excerpt: current.excerpt,
-        year: d[1].slice(2),
-        month: d[2],
-        day: d[3],
-        attachmentsUrl: current.attachments.length > 0 && current.attachments[0].images.large.url || "",
-        picnum: current.attachments.length
-    };
-}
-
-function renderHomePage(postResult) {
-    //渲染list
-    var renerData = [];
-    postResult.forEach(function(current,idx){
-        renerData.push( formatHomeData(current,idx));
-    });
-    var tpl = template(TPL, { data :renerData });
-    return tpl;
-}
-
-function renderBtns(){
-    typeof postResult[INDEX+1] == "undefined" ? $('.js-next').hide() :  $('.js-next').show();
-    typeof postResult[INDEX-1] == "undefined" ? $('.js-prev').hide() :  $('.js-prev').show();
-}
-
-function translates(dom, transitionSpeed, translate3dX) {
-    dom[0].style.webkitTransform = 'translate(' + translate3dX + 'px,0px) translateZ(0)';
-    dom[0].style.transform = 'translate(' + translate3dX + 'px, 0px) translateZ(0)';
-
-}
-
-function getTranslateX(elem){
-    var matrix = window.getComputedStyle(elem[0], null);
-    var transform = matrix['webkitTransform'] || matrix['transform'];
-    var split = transform.split(')')[0].split(', ');
-    var x = Math.round(+(split[12] || split[4]));
-    return x || 0;
-}
-
-var animateDiv = $('#container');
-function bindEvent(){
-    var panel = $('#panel');
-    var nav = $('.js-nav');
-    var thumb = $('.js-thumb');
-    panel.delegate('.js-next', 'click', function(e){
-        var next = INDEX+1;
-        var nextDom = animateDiv.find('.item[data-index="'+next+'"]');
-        //getMorePosts();
-        if(INDEX >=0 && next < postResult.length && nextDom.length>0){
-            ++INDEX;
-            renderBtns();
-            var _x = -getTranslateX(animateDiv)+nextDom.offset().left -100;
-            translates( animateDiv, 200, -_x);
-        }
-    }).delegate('.js-prev', 'click', function(e){
-        var prev = INDEX-1;
-        var prevDom = animateDiv.find('.item[data-index="'+prev+'"]');
-
-        if(prev >=0 && prevDom.length > 0){
-            --INDEX;
-            renderBtns();
-            var _x = -getTranslateX(animateDiv)+prevDom.offset().left-100;
-            translates( animateDiv, 200, -_x);
-        }
-    });
-
-    $(window).bind('resize',function(){
-        var currentDom = animateDiv.find('.item[data-index="'+INDEX+'"]');
-        if(currentDom.offset().left !== 100){
-            var _x = -getTranslateX(animateDiv) + currentDom.offset().left - 100;
-            translates( animateDiv, 200, -_x);
-        }
-    });
-
-    nav.bind('click', function (e) {
-        thumb.show();
-    });
-    thumb.delegate('.js-close', 'click', function(){
-        thumb.hide()
-    });
-}
-
 initPage();
-bindEvent();
