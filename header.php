67fb426a4bf6d03ea4fac16d3edaabc485a8dc2e
xiawei <evacuee.xia@gmail.com>
2016-05-16 09:49:11 +0800

    增加了网站tittle等信息

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width">
+<title><?php bloginfo('name'); ?></title>
+<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/iconfont/iconfont.css">
